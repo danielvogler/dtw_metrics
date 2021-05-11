ccf46ccd0b161958581e421a2bd0e855233e81f1
danielvogler <davogler@ethz.ch>
2021-05-11 22:03:55 +0200

    plotting functions

--- dtw_metrics.py
+++ dtw_metrics.py
@@ -97,6 +97,54 @@ def optimal_warping_path(acm):
     return owp
 
 
+def plot_sequences( reference, dataset ):
+
+    fig = plt.figure(num=None, figsize=(200, 150), dpi=80, facecolor='w', edgecolor='k')
+    p = plt.scatter(reference[0],reference[1],s=500,marker='.',c='k',label="Reference")
+    p = plt.scatter(dataset[0],dataset[1],s=500,marker='.',c='r',label="Dataset")
+    plt.legend(loc='upper center')
+    plt.xlabel("Time [-]")
+    plt.ylabel("Value [-]")
+
+    return
+
+
+def plot_cost_matrix(y_1, y_2):
+
+    ### cost matrix 
+    cm = cost_matrix(y_1,y_2)
+    ### dtw
+    D = acm( [y_1,x_1], [y_2,x_2] )
+    owp = optimal_warping_path( D )
+
+    # Set up the axes with gridspec
+    fig = plt.figure(figsize=(6, 6))
+    grid = plt.GridSpec(6, 6, hspace=0.2, wspace=0.2)
+    main_ax = fig.add_subplot(grid[:-1, 1:])
+    y_plot = fig.add_subplot(grid[:-1, 0], sharey=main_ax)
+    x_plot = fig.add_subplot(grid[-1, 1:], sharex=main_ax)
+
+    # scatter points on the main axes
+    main_ax.pcolormesh(cm)
+    main_ax.plot(owp[:,0],owp[:,1],color='w')
+    main_ax.yaxis.tick_right()
+    main_ax.xaxis.tick_top()
+    main_ax.set_title('Cost matrix')
+
+    # plots on the attached axes
+    x_plot.plot(np.linspace(0,len(y_2),len(y_2)), y_2, color='gray')
+    x_plot.invert_yaxis()
+    x_plot.set_ylim([-1.5,1.5])
+    x_plot.set_xlabel('Query [-]')
+    # y-axis
+    y_plot.plot(y_1, np.linspace(0,len(y_1),len(y_1)), color='gray')
+    y_plot.invert_xaxis()
+    y_plot.set_xlim([1.5,-1.5])
+    y_plot.set_ylabel('Reference [-]')
+
+    return
+    
+
 '''
     trig example
 '''
