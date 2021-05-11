a1bc939ce4bfaa3abf6c5c8c17ef2a295a5c0839
danielvogler <davogler@ethz.ch>
2021-05-11 22:03:22 +0200

    minimal example

--- dtw_metrics.py
+++ dtw_metrics.py
@@ -95,3 +95,19 @@ def optimal_warping_path(acm):
     owp = np.flip( owp )
 
     return owp
+
+
+'''
+    trig example
+'''
+### define sequence lengths
+length_1 = 100
+length_2 = 100
+
+### sequence 1
+x_1 = np.linspace(0, 4*pi, length_1)
+y_1 = np.cos(x_1)
+### sequence 2
+x_2 = np.linspace(0, 4*pi, length_2)
+distortion = np.random.uniform(low=0.8, high=1.0, size=( length_2, )) + np.cos(x_2/4*3) + np.cos(1.5*x_2)*0.2
+y_2 = np.cos(x_2) * distortion
