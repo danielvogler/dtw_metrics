c802f427b6effb2fa9cf3b866f5ead9489fb170e
danielvogler <davogler@ethz.ch>
2021-05-19 22:07:53 +0200

    optimal warping path plotting function update

--- dtwmetrics/dtwutils.py
+++ dtwmetrics/dtwutils.py
@@ -18,8 +18,8 @@ class DTWUtils:
     def plot_sequences(self, reference, dataset ):
 
         fig = plt.figure(num=None, figsize=(200, 150), dpi=80, facecolor='w', edgecolor='k')
-        p = plt.scatter(reference[1],reference[0],s=500,marker='.',c='k',label="Reference")
-        p = plt.scatter(dataset[1],dataset[0],s=500,marker='.',c='r',label="Dataset")
+        p = plt.scatter(reference[:,0],reference[:,1],s=500,marker='.',c='k',label="Reference")
+        p = plt.scatter(dataset[:,0],dataset[:,1],s=500,marker='.',c='r',label="Dataset")
         plt.legend(loc='upper center')
         plt.xlabel("Time [-]")
         plt.ylabel("Value [-]")
@@ -28,16 +28,11 @@ class DTWUtils:
 
 
     def plot_cost_matrix(self, reference, dataset ):
-
-        y_1 = reference[0]
-        x_1 = reference[1]
-        y_2 = dataset[0]
-        x_2 = dataset[1]
         
         ### cost matrix 
-        cm = dtwm.cost_matrix(y_1,y_2)
+        cm = dtwm.cost_matrix( reference[:,1] , dataset[:,1] )
         ### dtw
-        D = dtwm.acm( [y_1,x_1], [y_2,x_2] )
+        D = dtwm.acm( reference, dataset )
         owp = dtwm.optimal_warping_path( D )
 
         # Set up the axes with gridspec
@@ -49,18 +44,18 @@ class DTWUtils:
 
         # scatter points on the main axes
         main_ax.pcolormesh(cm)
-        main_ax.plot(owp[:,0],owp[:,1],color='w')
+        main_ax.plot(owp[:,1],owp[:,0],color='w')
         main_ax.yaxis.tick_right()
         main_ax.xaxis.tick_top()
         main_ax.set_title('Cost matrix')
 
         # plots on the attached axes
-        x_plot.plot(np.linspace(0,len(y_2),len(y_2)), y_2, color='gray')
+        x_plot.plot(np.linspace(0,len(dataset[:,1]),len(dataset[:,1])), dataset[:,1], color='gray')
         x_plot.invert_yaxis()
         x_plot.set_ylim([-1.5,1.5])
         x_plot.set_xlabel('Query [-]')
         # y-axis
-        y_plot.plot(y_1, np.linspace(0,len(y_1),len(y_1)), color='gray')
+        y_plot.plot( reference[:,1] , np.linspace(0,len(reference[:,1]),len(reference[:,1])), color='gray')
         y_plot.invert_xaxis()
         y_plot.set_xlim([1.5,-1.5])
         y_plot.set_ylabel('Reference [-]')
