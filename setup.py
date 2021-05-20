e970f18d98b93f7251fb3c6e2e73d49f72de438a
danielvogler <davogler@ethz.ch>
2021-05-20 11:17:49 +0200

    setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dtw_metrics',
+      version='0.0.1',
+      description='Dynamic time warping metrics',
+      long_description='Dynamic time warping metrics',
+      url='https://github.com/danielvogler/dtw_metrics',
+      author='Daniel Vogler',
+      author_email='geopard.py@gmail.com',
+      license='MIT',
+      packages=['dtw_metrics'],
+      install_requires=['scipy>=1.5.4'])
