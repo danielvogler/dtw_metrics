b27b6468c50d6febe931dfadb2f3f379163837c6
danielvogler <davogler@ethz.ch>
2021-05-28 00:42:32 +0200

    restructure cm etc

--- dtwmetrics/dtwmetrics.py
+++ dtwmetrics/dtwmetrics.py
@@ -18,121 +18,54 @@ from scipy.spatial.distance import cdist
 class DTWMetrics:
 
     ### cost matrix calculation
-    def cost_matrix_calculation(self, X, Y, distance_metric='cityblock'):
-
-        X = np.asarray(X, order='c')
-        Y = np.asarray(Y, order='c')
-
-        ### dimensions
-        X_sh = X.shape
-        Y_sh = Y.shape
-    
-        ### initialize
-        cm = np.empty( (X_sh[0], Y_sh[0]), dtype=np.double )
-
-        ### function string
-        dm_str = str('distance_' + distance_metric)
-        dm_func = getattr(self, dm_str)
-
-        ### create cost matrix
-        for i in range(0, X_sh[0] ):
-            for j in range(0, Y_sh[0] ):
-                cm[i, j] = dm_func(X[i], Y[j]) 
-
-        return cm
-
-
-    ### call cost matrix
-    def cm(self, reference, query, distance_metric='euclidean'):
+    def cm(self, X, Y, distance_metric='euclidean', method='cdist'):
 
         print('\tComputing cost matrix ({})\n'.format(distance_metric) )
 
-        reference = self.dim_check( reference )
-        query = self.dim_check( query )
+        X = self.dim_check( X )
+        Y = self.dim_check( Y )
 
-        ### compute cost matrix
-        #cm = self.cost_matrix_calculation(reference, query, distance_metric=distance_metric)
-        cm = cdist(reference, query, metric=distance_metric)
+        ### use scipy cdist
+        if method == 'cdist':
+
+            cm = cdist(X, Y, metric=distance_metric)
+
+        ### use own cost matrix calculation
+        else:
+
+            X = np.asarray(X, order='c')
+            Y = np.asarray(Y, order='c')
+
+            ### dimensions
+            X_sh = X.shape
+            Y_sh = Y.shape
+        
+            ### initialize
+            cm = np.empty( (X_sh[0], Y_sh[0]), dtype=np.double )
+
+            ### function string
+            dm_str = str('distance_' + distance_metric)
+            dm_func = getattr(self, dm_str)
+
+            ### create cost matrix
+            for i in range(0, X_sh[0] ):
+                for j in range(0, Y_sh[0] ):
+                    cm[i, j] = dm_func(X[i], Y[j]) 
 
         return cm
 
+            
+    ### check if 1D and convert to vector array if so
+    def dim_check(self, x):
 
-    ### accumulated cost matrix
-    def acm(self, reference, query, distance_metric='euclidean', step_pattern='symmetric_p0' ):
+        ### check dimensionality
+        x = np.atleast_2d( x )
 
-        print('\tComputing accumulated cost matrix ({})\n'.format(distance_metric) )
+        ### transform for cdist if 1D
+        if x.shape[0] == 1:
+            x = x.T
 
-        ### call cm
-        cm = self.cm(reference, query, distance_metric)
-
-        ### function string
-        step_pattern_str = str('step_' + step_pattern)
-        step_pattern_func = getattr(self, step_pattern_str)
-
-        ### execute step path
-        acm = step_pattern_func(cm)
-
-        return acm
-
-
-    ### optimal warping path owp
-    def optimal_warping_path(self, acm, b=None ):
-        
-        print('\tOptimal warping path (owp)')
-
-        ### determine acm shape
-        N, M = acm.shape
-
-        ### if subsequence owp requested
-        if b:
-            if b < M:
-                M = b
-            else:
-                print('Subsequence length must be below total array length')
-        else:
-            print('Matching entire query sequence')
-
-        ### move one entry in reverse
-        n = N - 1
-        m = M - 1
-
-        ### owp to be populated in reverse
-        p = []
-        p.append([N,M])
-
-        ### compute in reverse order
-        ### From (1) Algorithm: OptimalWarpingPath
-        while n > 0 and m > 0:
-            ### check if acm bounds are reached
-            if n == 1:
-                m = m - 1
-            elif m == 1:
-                n = n - 1
-            else:
-                ### compute direction of optimal step
-                optimal_step = []
-                optimal_step = np.argmin( [ acm[n-1,m-1], acm[n-1,m], acm[n,m-1] ] )
-                ### progress indices in direction of optimal step
-                if optimal_step == 0:
-                    n = n - 1
-                    m = m - 1
-                elif optimal_step == 1:
-                    n = n - 1
-                elif optimal_step == 2:
-                    m = m - 1
-                else:
-                    print('Error in optimal step computation')
-
-            ### append indices of optimal step
-            p.append([n,m])
-
-        ### B.C.
-        p.append([0,0])
-
-        owp = np.asarray( p )
-        owp = np.flip( owp )
-
-        return owp
+        return x
 
 
     ### manhattan or cityblock distance
@@ -151,26 +84,22 @@ class DTWMetrics:
         return dist
 
 
-    ### check if 1D and convert to vector array if so
-    def dim_check(self, x):
+    ### accumulated cost matrix
+    def acm(self, reference, query, distance_metric='euclidean', step_pattern='symmetric_p0' ):
 
-        ### check dimensionality
-        x = np.atleast_2d( x )
+        print('\tComputing accumulated cost matrix ({})\n'.format(distance_metric) )
 
-        ### transform for cdist if 1D
-        if x.shape[0] == 1:
-            x = x.T
+        ### call cm
+        cm = self.cm(reference, query, distance_metric)
 
-        return x
+        ### function string
+        step_pattern_str = str('step_' + step_pattern)
+        step_pattern_func = getattr(self, step_pattern_str)
 
+        ### execute step path
+        acm = step_pattern_func(cm)
 
-    ### warped sequence
-    def warped_sequence(self, sequence, owp):
-
-        warped_sequence = [ [ owp[i,1], sequence[ owp[i,0] ] ] for i in range( owp.shape[0] - 1 ) ]
-        warped_sequence = np.asarray( warped_sequence )
-
-        return warped_sequence
+        return acm
 
 
     ### step pattern: symmetric p0
@@ -255,3 +184,72 @@ class DTWMetrics:
                 acm[n, m] = min( acm[n-1, m-1], acm[n-2, m-1], acm[n-1, m-2]) + cm[n, m]
 
         return acm
+
+
+    ### optimal warping path owp
+    def optimal_warping_path(self, acm, b=None ):
+        
+        print('\tOptimal warping path (owp)')
+
+        ### determine acm shape
+        N, M = acm.shape
+
+        ### if subsequence owp requested
+        if b:
+            if b < M:
+                M = b
+            else:
+                print('Subsequence length must be below total array length')
+        else:
+            print('Matching entire query sequence')
+
+        ### move one entry in reverse
+        n = N - 1
+        m = M - 1
+
+        ### owp to be populated in reverse
+        p = []
+        p.append([N,M])
+
+        ### compute in reverse order
+        ### From (1) Algorithm: OptimalWarpingPath
+        while n > 0 and m > 0:
+            ### check if acm bounds are reached
+            if n == 1:
+                m = m - 1
+            elif m == 1:
+                n = n - 1
+            else:
+                ### compute direction of optimal step
+                optimal_step = []
+                optimal_step = np.argmin( [ acm[n-1,m-1], acm[n-1,m], acm[n,m-1] ] )
+                ### progress indices in direction of optimal step
+                if optimal_step == 0:
+                    n = n - 1
+                    m = m - 1
+                elif optimal_step == 1:
+                    n = n - 1
+                elif optimal_step == 2:
+                    m = m - 1
+                else:
+                    print('Error in optimal step computation')
+
+            ### append indices of optimal step
+            p.append([n,m])
+
+        ### B.C.
+        p.append([0,0])
+
+        owp = np.asarray( p )
+        owp = np.flip( owp )
+
+        return owp
+
+
+    ### warped sequence
+    def warped_sequence(self, sequence, owp):
+
+        warped_sequence = [ [ owp[i,1], sequence[ owp[i,0] ] ] for i in range( owp.shape[0] - 1 ) ]
+        warped_sequence = np.asarray( warped_sequence )
+
+        return warped_sequence
